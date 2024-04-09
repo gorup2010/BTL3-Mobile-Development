a0954c54bf8c01cae5d34a8ad166e91ca466ace2
Hoi Lau <lauhoi2010@gmail.com>
2024-04-09 20:40:58 +0700

    Add boarding content

--- screens/OnboardingScreen.js
+++ screens/OnboardingScreen.js
@@ -13,20 +13,20 @@ export default function OnboardingScreen() {
           {
             backgroundColor: "#fff",
             image: <Boarding1 />,
-            title: "Onboarding",
-            subtitle: "Done with React Native Onboarding Swiper",
+            title: "Biết tiền đi về đâu với\n báo cáo thu chi",
+            subtitle: "Cung cấp báo cáo thu chi theo tháng, \ntuần, ngày đúng yêu cầu của bạn, giúp \ndễ dàng theo dõi và lập kế hoạch trong \ntương lai",
           },
           {
             backgroundColor: "#fff",
-            image: <Text>Trang 2</Text>,
-            title: "Onboarding",
-            subtitle: "Done with React Native Onboarding Swiper",
+            image: <Boarding2 />,
+            title: "Cùng nhau hợp tác \ncùng nhau tiến lên",
+            subtitle: "Chia sẻ kế hoạch thu chi cá nhân với \nbạn bè xung quanh để cùng nhau xây \ndựng kế hoạch chi tiêu một cách hợp lí \nvà hiệu quả",
           },
           {
             backgroundColor: "#fff",
-            image: <Text>Trang 3</Text>,
-            title: "Onboarding",
-            subtitle: "Done with React Native Onboarding Swiper",
+            image: <Boarding3 />,
+            title: "Đặt kế hoạch thu chi \ntránh dùng quá tay ",
+            subtitle: "Đặt mục tiêu thu chi giúp bạn kiểm soát \nlượng tiền sử dụng. Tự động thông báo \nnếu chi tiêu vượt mức kế hoạch",
           },
         ]}
       />
